82b7f8b99d7cbe98ceb476698f768b7e706563b9
quoclam204 <nguyenlequoclam@gmail.com>
2025-12-27 12:15:43 +0700

    Update page

--- client/src/PortfolioContainer/ContactMe/ContactMe.js
+++ client/src/PortfolioContainer/ContactMe/ContactMe.js
@@ -72,20 +72,17 @@ export default function ContactMe(props) {
           <h2 className="title">
             <Typical loop={Infinity} steps={["Get In Touch 📧", 1000]} />
           </h2>{" "}
-          <a href="https://web.facebook.com/?_rdc=1&_rdr">
+          <a href="https://www.facebook.com/quoclam.nguyen.52643/">
             <i className="fa fa-facebook-square" />
           </a>
-          <a href="#">
-            <i className="fa fa-google-plus-square" />
+          <a href="https://github.com/quoclam204/">
+            <i className="fa fa-github" />
           </a>
-          <a href="https://www.instagram.com/instructor_ehizeex/">
+          <a href="https://www.linkedin.com/in/your-linkedin-username/">
+            <i className="fa fa-linkedin-square" />
+          </a>
+          <a href="https://www.instagram.com/q_lam204?igsh=MWcwbXdndTBubHhkeA==/">
             <i className="fa fa-instagram" />
-          </a>
-          <a href="https://www.youtube.com/channel/UCSSr5ZDFbilpZ592_ycoAwA">
-            <i className="fa fa-youtube-square" />
-          </a>
-          <a href="https://twitter.com/Ehiedu_baba">
-            <i className="fa fa-twitter" />
           </a>
         </div>
         <div className="back-form">
